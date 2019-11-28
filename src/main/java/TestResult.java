6ccd170a892d5bb6d725804126ac548d50812dc1
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-28 22:49:41 +0300

    Scripted auto-commit on change (2019-11-28 22:49:41) by gitwatch.sh

--- src/main/java/TestResult.java
+++ src/main/java/TestResult.java
@@ -1,2 +1,18 @@
 public class TestResult {
+
+        private String URL;
+        private int count;
+
+        public TestResult(String Url, int count ) {
+            this.URL = URL;
+            this.count = count;
+        }
+        public String getURL() {
+            return URL;
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
 }
