01963cc4a3faf0c16aa2ef686d39d5fe813c0f8b
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-28 22:15:03 +0300

    Scripted auto-commit on change (2019-11-28 22:15:03) by gitwatch.sh

--- src/main/java/actorSystem.java
+++ src/main/java/actorSystem.java
@@ -1,2 +1,24 @@
 public class actorSystem {
+
+        private HashMap  <Integer, ArrayList<Test>> store = new HashMap<>();
+        //id res
+        @Override
+        public Receive createReceive() {
+            return ReceiveBuilder.create()
+                    .match(StoreMassage.class, m -> {
+                        if (!store.containsKey(m.getPackageID()))
+                            store.put(m.getPackageID(), m.getTests());
+                        else {
+                            ArrayList<Test> res = store.get(m.getPackageID());
+                            res.addAll(m.getTests());
+                            store.replace(m.getPackageID(), res);
+                        }
+                        System.out.println("receive message! " + m);
+                    })
+                    .match(GetMessage.class,  req -> { sender().tell(
+                            new StoreMassage(req.getPackageID(), store.get(req.getPackageID())), self());
+                    }).build();
+    }
+
+
 }
