6e82c78b7863fceffb9e6a2771f107d5d8af3ab3
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-30 05:14:21 +0300

    Scripted auto-commit on change (2019-11-30 05:14:21) by gitwatch.sh

--- src/main/java/actorSystem.java
+++ src/main/java/actorSystem.java
@@ -16,7 +16,7 @@ public class actorSystem {
                     .match(SearchResult.class, m -> {
                         String url = m.getURL();
                         int count - m.getCount();
-                            if(store.containsKey() && store.)
+                            if(store.containsKey() && store.get(url).)
                     })
                     .match(TestResult.class, m -> {
                         store.put(m.getURL(), m.getTime());
