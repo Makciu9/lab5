5e492f29edc3acec90eb18c50cddfb93d8339c34
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-11-30 06:17:10 +0300

    Scripted auto-commit on change (2019-11-30 06:17:10) by gitwatch.sh

--- src/main/java/ServerResult.java
+++ src/main/java/ServerResult.java
@@ -1,17 +1,13 @@
-import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.http.javadsl.Http;
-import akka.http.javadsl.model.HttpRequest;
-import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
-import akka.stream.javadsl.Flow;
 
 public class ServerResult {
     ActorRef actorSystem;
-    static Flow<HttpRequest, HttpResponse, NotUsed> ServerResult(){
-        system.actorOf(Props.create(actorSystem.class)
+    static void ServerResult(Http http, ActorSystem system, ActorMaterializer materializer){
+        system.actorOf(Props.create(actorSystem.class);
 
     }
 }
