a14bdd366ea1543fe12877806a8ee886f66b4c2a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2019-12-06 23:29:43 +0300

    Scripted auto-commit on change (2019-12-06 23:29:43) by gitwatch.sh

--- src/main/java/actorSystem.java
+++ src/main/java/actorSystem.java
@@ -14,14 +14,16 @@ public class actorSystem extends AbstractActor {
         @Override
         public AbstractActor.Receive createReceive() {
             return ReceiveBuilder.create()
-                    .match(SearchResult.class, m -> {
+                    .match(TestResult.class, m -> {
 
                         String url = m.getURL();
-                        int count = m.getCount();
+                        Long count = (long) m.getURL();
                         System.out.print(url);
                         System.out.print(count);
-                        if ()
-                           sender().tell(new SearchResult(m.getURL(), count), self());
+                        if (store.containsKey(m.getURL())) {
+                            sender().tell(new SearchResult(m.getURL(), count), self());
+                        }
+                        else sender().tell(new SearchResult(m.getURL(), -1l), self())
 
                     })
                     .match(TestResult.class, m -> {
